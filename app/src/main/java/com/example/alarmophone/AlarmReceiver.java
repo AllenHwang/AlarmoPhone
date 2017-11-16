2799e8837a91de7f9907245b744fdb4085957fef
Jose Camacho <josecam10596@gmail.com>
2017-11-16 00:28:45 -0600

    Got basic notification. Working on master woops, switching to timer after this. - Jose

--- app/src/main/java/com/example/alarmophone/AlarmReceiver.java
+++ app/src/main/java/com/example/alarmophone/AlarmReceiver.java
@@ -4,23 +4,37 @@ package com.example.alarmophone;
  * Created by Phoenix Horizon on 10/31/2017.
  */
 
+import android.annotation.TargetApi;
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static com.example.alarmophone.NotificationPublisher.NOTIFICATION;
+import static com.example.alarmophone.NotificationPublisher.NOTIFICATION_ID;
+
 public class AlarmReceiver extends BroadcastReceiver {
 
 
+    private String CHANNEL_ID = "Alarm_Channel";
 
+    @TargetApi(Build.VERSION_CODES.O)
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(final Context context, Intent intent) {
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
@@ -35,12 +49,36 @@ public class AlarmReceiver extends BroadcastReceiver {
         msgStr.append(formatter.format(new Date()));
         Toast.makeText(context, msgStr, Toast.LENGTH_LONG).show();
         AlarmActivity.ringtone.play();
+
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, "Notification", importance);
+        mChannel.setLightColor(Color.RED);
+        mChannel.enableVibration(true);
+        mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        notificationManager.createNotificationChannel(mChannel);
+
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        notificationManager.notify(id, notification);
+
     }
 
     public void SetAlarm(Context context, Long time){
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, AlarmReceiver.class);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("Scheduled Notification")
+                .setContentText("Alarm!")
+                .setSmallIcon(R.drawable.ic_launcher_background);
+        Notification notification = builder.build();
+        intent.putExtra(NOTIFICATION_ID, 1);
+        intent.putExtra(NOTIFICATION, notification);
+
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         am.set(AlarmManager.RTC_WAKEUP, time, pi);
     }
 
