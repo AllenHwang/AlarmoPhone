439996ba629b3dbc81884bd8a85111a8c8be3b57
Andriy Dashko <andriydar@utexas.edu>
2017-12-03 22:27:52 -0600

    fixed possible merge issues

--- app/src/main/java/com/example/alarmophone/MainMenuActivity.java
+++ app/src/main/java/com/example/alarmophone/MainMenuActivity.java
@@ -5,12 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainMenuActivity extends AppCompatActivity {
+
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Main Menu");
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main_menu);
     }
     /**Called when the user presses the Alarms button */
@@ -19,8 +25,14 @@ public class MainMenuActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
-    public void timerActivity(View view){
-        Intent intent = new Intent(this, TimerActivity.class);
+    public void timersActivity(View view){
+        Intent intent = new Intent(this, TimersActivity.class);
+        startActivity(intent);
+    }
+    public void logoutActivity(View view){
+        mAuth.signOut();
+        finish();
+        Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
     }
 }
